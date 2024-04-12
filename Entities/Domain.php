415eb014a9e6db6344beb1fbbe575f697fcb0673
Dedan <dedanirungu@gmail.com>
2024-04-12 11:13:15 +0300

    Add rights() method to Domain, Price, Registrar, Subdomain, and Subdomaindomain models

--- Entities/Domain.php
+++ Entities/Domain.php
@@ -95,7 +95,13 @@ class Domain extends BaseModel
      */
     public function rights(): array
     {
+        $rights = parent::rights();
 
+        $rights['staff'] = ['view' => true];
+        $rights['registered'] = ['view' => true];
+        $rights['guest'] = [];
+
+        return $rights;
     }
 
 }
