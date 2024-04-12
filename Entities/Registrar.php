537bd44e3fe233b71e1bd900558ecb5e462a8c01
Dedan <dedanirungu@gmail.com>
2024-04-12 12:16:52 +0300

    Changes to files: Entities/Registrar.php Entities/Subdomain.php

--- Entities/Registrar.php
+++ Entities/Registrar.php
@@ -81,7 +81,7 @@ class Registrar extends BaseModel
         $rights = parent::rights();
 
         $rights['staff'] = ['view' => true];
-        $rights['registered'] = ['view' => true];
+        $rights['registered'] = [];
         $rights['guest'] = [];
 
         return $rights;
