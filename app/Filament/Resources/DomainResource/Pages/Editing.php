99edfb9be8712f2f090043fc625e86c60244bda1
Dedan <dedanirungu@gmail.com>
2024-10-13 16:28:14 +0300

    Release 2024.14

---
+++ app/Filament/Resources/DomainResource/Pages/Editing.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Domain\Filament\Resources\DomainResource\Pages;
+
+use Modules\Base\Filament\Resources\Pages\EditingBase;
+
+// Class List that extends ListBase
+class Editing extends EditingBase
+{
+}
---
+++ app/Filament/Resources/SubdomainResource/Pages/Creating.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Subdomain\Filament\Resources\SubdomainResource\Pages;
+
+use Modules\Base\Filament\Resources\Pages\CreatingBase;
+
+// Class List that extends ListBase
+class Creating extends CreatingBase
+{
+}
